dd97bd4d22ce79d63b50f254c2127cf9da9e6b4a
sanikumar <sanikumar.sahani@unvired.io>
2022-09-27 14:48:51 +0530

    react-redux-toolkit

---
+++ index.js
@@ -0,0 +1,32 @@
+const redux = require('redux')
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+/**
+ * function is ACTION creater
+ * @returns an object
+ */
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1,
+    }
+}
+
+const initialState = { numOfCakes: 10, }
+
+/**
+ * 
+ * @param {*} state 
+ * @param {*} action 
+ * @returns updated state
+ */
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1,
+            }
+        default: return state
+    }
+}
