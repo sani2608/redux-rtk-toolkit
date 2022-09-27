d8d87a0600a13fc9489d386daed64f046f8d8375
sanikumar <sanikumar.sahani@unvired.io>
2022-09-27 15:14:37 +0530

    redux full sample

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const redux = require('redux')
+const createStore = redux.createStore;
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 /**
@@ -30,3 +31,16 @@ const reducer = (state = initialState, action) => {
         default: return state
     }
 }
+
+
+const store = createStore(reducer);
+
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('subscribe', store.getState()));
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
