ef3321a0fcdc2b80a3240cce3db4b802e5872a5b
sanikumar <sanikumar.sahani@unvired.io>
2022-09-27 15:28:21 +0530

    restock

--- index.js
+++ index.js
@@ -9,9 +9,22 @@ const CAKE_ORDERED = "CAKE_ORDERED";
 function orderCake() {
     return {
         type: CAKE_ORDERED,
-        quantity: 1,
+        payload: 1,
     }
 }
+
+// SCENARIOS
+//? 1. RESTOCKING CAKES 
+
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+
+function restockCake(qty = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty
+    }
+}
+
 
 const initialState = { numOfCakes: 10, }
 
@@ -27,6 +40,11 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 numOfCakes: state.numOfCakes - 1,
+            } 
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload
             }
         default: return state
     }
@@ -42,5 +60,12 @@ const unsubscribe = store.subscribe(() => console.log('subscribe', store.getStat
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+
+
+store.dispatch(restockCake(5));
+store.dispatch(restockCake(5));
+store.dispatch(restockCake(3));
+
+
 
 unsubscribe();
